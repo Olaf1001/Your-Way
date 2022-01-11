c741286f6e6752dfa4b119a35b3bea05c94b9363
Olaf1001 <olaf1001.front@gmail.com>
2022-01-11 14:39:07 +0100

    Add mobile menu

---
+++ scripts/app.js
@@ -0,0 +1,36 @@
+const hamburgerMenuIcon = document.querySelector('.mobile-menu');
+const htmlTag = document.querySelector('html');
+const bodyTag = document.querySelector('body');
+const mobileMenu = document.querySelector('.mobile-nav');
+const mobileLinks = document.querySelectorAll('.mobile-links a')
+
+document.addEventListener('DOMContentLoaded', () => {
+    hamburgerMenuIcon.addEventListener('click' , () => {
+        hamburgerMenuIcon.classList.toggle('mobile-menu-icon-active');
+        htmlTag.classList.toggle('scroll-disable');
+        bodyTag.classList.toggle('scroll-disable');
+        mobileMenu.classList.toggle('mobile-nav-active');
+    })
+
+    for (let i = 0; i < mobileLinks.length; i++) {
+        mobileLinks[i].addEventListener("click", () => {
+            htmlTag.classList.toggle('scroll-disable');
+            bodyTag.classList.toggle('scroll-disable');
+            hamburgerMenuIcon.classList.toggle('mobile-menu-icon-active');
+            mobileMenu.classList.toggle('mobile-nav-active');
+        });
+    }
+})
+
+$(function() {
+    $('a[href*=\\#]:not([href=\\#])').on('click', function() {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.substr(1) +']');
+        if (target.length) {
+            $('html,body').animate({
+                scrollTop: target.offset().top
+            }, 1000);
+            return false;
+        }
+    });
+});
